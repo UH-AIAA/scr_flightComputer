6d9466dad21461300f1139234b526bb16eb69699
Nathan Samuell <nathansamuell@duck.com>
2024-09-04 23:23:58 -0500

    added library requirements install script for arduino-cli usage

---
+++ library_requirements.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+arduino-cli lib install SD
+arduino-cli lib install "Adafruit Unified Sensor"
+arduino-cli lib install "Adafruit GPS Library"
+arduino-cli lib install "Adafruit ADXL375"
+arduino-cli lib install "Adafruit BMP3XX Library"
+arduino-cli lib install "Adafruit LSM6DS"
+arduino-cli lib install "Adafruit BNO055"
