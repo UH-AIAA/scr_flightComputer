e0ade59d61c299d87f0f78c18c2946a7da82c698
nathansamuell <nathansamuell@duck.com>
2024-09-09 10:36:22 -0500

    fixed teensy library issues--code compiles now

--- library_requirements.sh
+++ library_requirements.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+
+arduino-cli config init
+arduino-cli config add board_manager.additional_urls https://www.pjrc.com/teensy/package_teensy_index.json
+arduino-cli update
+arduino-cli core install teensy:avr
 arduino-cli lib install SD
 arduino-cli lib install "Adafruit Unified Sensor"
 arduino-cli lib install "Adafruit GPS Library"
