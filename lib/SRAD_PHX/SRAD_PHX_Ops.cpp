5bdf2d88ade809d466521f7dee335d9c432f4f1e
nathansamuell <nathansamuell@duck.com>
2025-03-12 17:53:16 -0500

    added license updates

--- lib/SRAD_PHX/SRAD_PHX_Ops.cpp
+++ lib/SRAD_PHX/SRAD_PHX_Ops.cpp
@@ -1,3 +1,13 @@
+/* SRAD Avionics Flight Software for AIAA-UH
+ *
+ * Copyright (c) 2024 Nathan Samuell + Dedah + Thanh! (www.github.com/nathansamuell, www.github.com/UH-AIAA)
+ *
+ * More information on the MIT license as well as a complete copy
+ * of the license can be found here: https://choosealicense.com/licenses/mit/
+ *
+ * All above text must be included in any redistribution.
+ */
+
 #include "SRAD_PHX.h"
 
 void incrementTime() {
